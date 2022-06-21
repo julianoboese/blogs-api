38b62062175b91988f10c93dbbcca4550bd1396e
Juliano Boese <juliano.boese@gmail.com>
2022-06-21 16:22:36 -0300

    feat(mod): :sparkles: create the blog posts model
    req10 - COMPLETE

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      {foreignKey: 'userId', as: 'Users'}
+    )
+  }
+
+  return BlogPost;
+}
