fb454d55b77c0aeeb3339ac4380c9bf365837b09
Juliano Boese <juliano.boese@gmail.com>
2022-06-21 15:12:11 -0300

    feat(usersGet): :sparkles: create GET /user route
    req05 - COMPLETE

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { userValidation } = require('../middlewares');
+const { userValidation, authorization } = require('../middlewares');
 const UserController = require('../controllers/UserController');
 
 const routes = express.Router();
 
 routes.post('/', userValidation, UserController.createUser);
+
+routes.use(authorization);
+routes.get('/', UserController.findUsers);
 
 module.exports = routes;
