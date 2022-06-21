3a0557ac5203bfc31f8d3c2a2cc1a3fc7d8e9765
Juliano Boese <juliano.boese@gmail.com>
2022-06-21 15:08:34 -0300

    feat(usersGet): :sparkles: create the findUsers service function
    req05 - WIP

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,12 @@
 const createError = require('http-errors');
 const { User } = require('../database/models');
 
+async function findUsers() {
+  const users = await User.findAll({ attributes: { exclude: ['password'] } });
+
+  return users;
+}
+
 async function createUser(userData) {
   const user = await User.findOne({ where: { email: userData.email } });
 
@@ -12,5 +18,6 @@ async function createUser(userData) {
 }
 
 module.exports = {
+  findUsers,
   createUser,
 };
