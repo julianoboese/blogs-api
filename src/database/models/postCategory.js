370f8c4932ffe17b89d36f4bd2c2c352de1a1b56
Juliano Boese <juliano.boese@gmail.com>
2022-06-21 16:32:24 -0300

    feat(mod): :sparkles: create the posts categories model
    req11 - COMPLETE

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    }
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+        as: 'BlogPosts',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId'
+    }),
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    })
+  }
+
+  return PostCategory;
+}
