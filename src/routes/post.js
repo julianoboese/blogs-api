92e4ece2517e41d99931974397ab18668ef44452
Juliano Boese <juliano.boese@gmail.com>
2022-06-22 18:04:29 -0300

    feat(postPut): :sparkles: create the PUT /post/:id route
    req15 - COMPLETE

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { authorization, createPostValidation } = require('../middlewares');
+const { authorization, createPostValidation, updatePostValidation } = require('../middlewares');
 const PostController = require('../controllers/PostController');
 
 const routes = express.Router();
@@ -8,5 +8,6 @@ routes.use(authorization);
 routes.get('/', PostController.findPosts);
 routes.get('/:id', PostController.findPost);
 routes.post('/', createPostValidation, PostController.createPost);
+routes.put('/:id', updatePostValidation, PostController.updatePost);
 
 module.exports = routes;
