837a2457f3f31480c59f1a65d795f82c8e34887f
Juliano Boese <juliano.boese@gmail.com>
2022-06-22 19:16:21 -0300

    style(loginPost): :fire: remove console.log

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -5,7 +5,6 @@ async function login(req, res) {
   const user = await LoginService.login(req.body);
 
   const token = signToken(user);
-  console.log(token);
 
   res.status(200).json({ token });
 }
