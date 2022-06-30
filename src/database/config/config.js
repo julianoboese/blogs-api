bceb07c94fb732796b46c13fbc2caeee513ae051
Juliano Boese <juliano.boese@gmail.com>
2022-06-30 11:08:43 -0300

    refactor(db): :card_file_box: change from mysql to postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,12 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DB_NAME,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
   },
@@ -27,6 +18,9 @@ module.exports = {
     ...options,
   },
   test: {
+    ...options,
+  },
+  production: {
     ...options,
   },
 };
