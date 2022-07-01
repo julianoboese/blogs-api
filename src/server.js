5c400054eda9be9564de0a56169aa228422f1a5c
Juliano Boese <juliano.boese@gmail.com>
2022-07-01 01:19:17 -0300

    feat: :sparkles: install and configure swagger

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerConfig = require('../swagger.json');
 const app = require('./api');
 const { errorMiddleware } = require('./middlewares');
 const { loginRoutes, userRoutes, categoriesRoutes, postRoutes } = require('./routes');
@@ -7,15 +9,16 @@ const { loginRoutes, userRoutes, categoriesRoutes, postRoutes } = require('./rou
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
 
-// não remova esse endpoint
-app.get('/', (_request, response) => {
-  response.send();
-});
-
 app.use('/login', loginRoutes);
 app.use('/user', userRoutes);
 app.use('/categories', categoriesRoutes);
 app.use('/post', postRoutes);
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+// não remova esse endpoint
+app.get('/', (_request, response) => {
+  response.send();
+});
 
 app.use(errorMiddleware);
 
